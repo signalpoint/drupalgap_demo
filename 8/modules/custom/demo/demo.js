37e3a97ed3a0ebef910ef589187dc537713c0671
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-02-05 03:15:40 -0500

    Many more improvements.

--- 8/modules/custom/demo/demo.js
+++ 8/modules/custom/demo/demo.js
@@ -21,284 +21,265 @@ demo.map = null;
 demo.routing = function() {
   var routes = {};
 
-  /**
-   * THE WELCOME PAGE
-   */
-
-  // The demo welcome page.
+  // Welcome page route.
   routes["demo.welcome"] = {
     "path": "/welcome",
     "defaults": {
       "_title": "Welcome",
-      "_controller": function() {
-        return new Promise(function(ok, err) {
-
-          // Grab the current user.
-          var account = dg.currentUser();
-
-          // Let's build a render element to render on the page...
-          var element = { };
-
-          switch (dg.config('theme').name) {
-
-            // "Out of the box" theme.
-            case 'ava':
-                if (account.isAnonymous()) {
-                  element.intro = {
-                    _theme: 'item_list',
-                    _type: 'ol',
-                    _items: [
-                      dg.t('Acknowledge how boring this looks'),
-                      dg.t('Realize DrupalGap is an empty canvas for app developers'),
-                      dg.t('Get "out of the box" to explore the tool set and extensions')
-                    ]
-                  };
-                }
-                else {
-                  var msg = dg.t('To support other CSS Frameworks (or no framework at all), the DrupalGap front end is very flexible.');
-                  element.intro = { _markup: '<blockquote>' + msg + '</blockquote>' };
-                }
-
-              break;
-
-            // Core theme for Bootstrap.
-            case 'burrito':
-              var msg = dg.t('Instantly add a _bootstrap front end to your app with a _module extension for DrupalGap.', {
-                _bootstrap: dg.l(dg.t('Bootstrap'), 'http://getbootstrap.com/'),
-                _module: dg.l(dg.t('module'), 'http://drupalgap.org/project/bootstrap')
-              });
-              element.intro = { _markup: '<blockquote>' + msg + '</blockquote>' };
-              break;
-
-            // Core theme for Foundation.
-            case 'frank':
-              var msg = dg.t('Instantly add a _foundation front end to your app with a _module extension for DrupalGap.', {
-                _foundation: dg.l(dg.t('Foundation'), 'http://foundation.zurb.com/'),
-                _module: dg.l(dg.t('module'), 'http://drupalgap.org/project/foundation')
-              });
-              element.intro = { _markup: '<blockquote>' + msg + '</blockquote>' };
-              break;
-
-          }
-
-          // List out some of DrupalGap's best features and how to get started.
-          if (dg.config('theme').name != 'ava' || account.isAuthenticated()) {
-
-            element.features = {
-              _markup:
-
-                '<h2>' + dg.t('Tools and Features') + '</h2>' +
-                '<blockquote>' + dg.t('By utilizing familiar coding syntax and concepts from Drupal 8 such as...') + '</blockquote>' +
-                dg.theme('item_list', {
-                  _items: [
-                    dg.t('Entities / Fields'),
-                    dg.t('Modules'),
-                    dg.t('Hooks'),
-                    dg.t('Themes'),
-                    dg.t('Regions'),
-                    dg.t('Blocks'),
-                    dg.t('Templates'),
-                    dg.t('Routes / Custom Pages'),
-                    dg.t('Forms API'),
-                    dg.t('User Roles / Permissions')
-                  ]
-                }) +
-              '<blockquote>' + dg.t('with built in pages, widgets and forms to handle...') + '</blockquote>' +
-              dg.theme('item_list', {
-                _items: [
-                  dg.t('User Authentication'),
-                  dg.t('Adding / Editing Entities'),
-                  dg.t('Displaying Entities / Fields'),
-                  dg.t('Rendering Views Result Data')
-                ]
-              }) +
-              '<blockquote>' + dg.t('the DrupalGap tool set is dedicated to Drupal 8 application development.') + '</blockquote>' +
-
-              '<h2>' + dg.t('Continuing the Demo') + '</h2>' +
-              '<blockquote>' + dg.t('Be sure to say hello on the _map and browse the _messages list.', {
-                  _map: dg.l(dg.t('map'), 'map'),
-                  _messages: dg.l(dg.t('messages'), 'messages')
-                }) + '</blockquote>' +
-
-              '<h2>' + dg.t('Getting Started') + '</h2>' +
-              '<blockquote>' + dg.t('Try the _helloWorld World for DrupalGap.', {
-                _helloWorld: dg.l(dg.t('Hello World'), 'http://docs.drupalgap.org/8/Hello_World')
-              }) + '</blockquote>'
-            };
-
-          }
-
-          ok(element);
-
-        });
-      }
+      "_controller": demo.welcomePage
     }
   };
 
-  /**
-   * THE MAP PAGE
-   */
-
-  // My map page route.
+  // Map page route.
   routes["demo.map"] = {
     "path": "/map",
     "defaults": {
       "_title": "Map",
-      "_controller": function() {
-        return new Promise(function(ok, err) {
-
-          // Grab the current user.
-          var account = dg.currentUser();
-
-          // Let's build a render element to render on the page...
-          var element = {};
-
-          // Google map.
-          element.map = demo.getMapRenderElement();
-          element.map._postRender.push(function() {
-
-            // Add clickable markers on the map for recent messages sent, then set the center of the map.
-            jDrupal.viewsLoad('articles').then(function(view) {
-              var results = view.getResults();
-              var markers = [];
-              for (var i = 0; i < results.length; i ++) {
-                var node = new jDrupal.Node(results[i]);
-                var marker = new google.maps.Marker({
-                  position: new google.maps.LatLng(
-                    node.get('field_latitude', 0).value,
-                    node.get('field_longitude', 0).value
-                  ),
-                  map: demo.map,
-                  icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
-                });
-                markers.push(marker);
-                var content = dg.l(node.getTitle(), 'node/' + node.id());
-                var infowindow = new google.maps.InfoWindow();
-                google.maps.event.addListener(marker, 'click', (function(marker, content, infowindow) {
-                  return function() {
-                    infowindow.setContent(content);
-                    infowindow.open(demo.map, marker);
-                  };
-                })(marker, content, infowindow));
-              }
-              var bounds = new google.maps.LatLngBounds();
-              for (var i = 0; i < markers.length; i++) { bounds.extend(markers[i].getPosition()); }
-              demo.map.fitBounds(bounds);
-            });
-
-          });
-
-          // Add a placeholder for showing messages.
-          element['message'] = {
-            _markup: '<div id="demo-message"></div>',
-            _weight: 2
-          };
-
-          // Anonymous users...
-          if (!account.isAuthenticated()) {
-
-            // Direct them to the login form so they can use the map.
-            element['message']._postRender = [function() {
-              var nid = window.localStorage.getItem('demo_message_sent');
-              demo.setMessage({
-                _theme: 'message',
-                _message: dg.t('Login to add a greeting to the map.')
-              });
-            }];
-            ok(element);
-
-          }
-
-          else {
-
-            // Authenticated users...
-
-            // If they've already sent a message, let them know and don't show the form.
-            if (window.localStorage.getItem('demo_message_sent')) {
-              element['message']._postRender = [function() {
-                var nid = window.localStorage.getItem('demo_message_sent');
-                demo.setMessage({
-                  _theme: 'message',
-                  _type: 'warning',
-                  _message: dg.t('You already sent a message!') + ' (' + dg.l(dg.t('view message'), 'node/' + nid) + ')'
-                });
-              }];
-              ok(element);
-              return;
-            }
-
-            // Show a message informing the user how to use the map and form.
-            element['message']._postRender = [function() {
-              demo.setMessage({
-                _theme: 'message',
-                _type: 'info',
-                _message:
-                dg.t('Click on the map and enter your message below...') + ' ' +
-                dg.l(dg.t('or use your current location'), null, {
-                  _attributes: {
-                    href: '',
-                    onclick: 'demo.getCurrentLocation(); return false;'
-                  }
-                })
-              });
-            }];
-
-            // Load the form, add it to DrupalGap, then attach its html to our render element.
-            dg.addForm('DemoSayHelloForm', dg.applyToConstructor(DemoSayHelloForm)).getForm().then(function(formHTML) {
-              element['say_hello_form'] = {
-                _markup: formHTML,
-                _weight: 2
-              };
-              ok(element);
-            });
-
-          }
-
-        });
-      }
+      "_controller": demo.mapPage
     }
   };
 
-  /**
-   * THE MESSAGES PAGE
-   */
-
-  // My messages page route.
+  // Messages page route.
   routes["demo.messages"] = {
     "path": "/messages",
     "defaults": {
       "_title": "Messages",
-      "_controller": function() {
-        return new Promise(function(ok, err) {
-
-          // Grab the current user.
-          var account = dg.currentUser();
-
-          // Let's build a render element to render on the page...
-          var element = {};
-
-          // Display a View listing of recent greetings.
-          element['article_list'] = dg.recentGreetings();
-
-          // Anonymous users...
-          if (!account.isAuthenticated()) {
-
-          }
-
-          else {
-
-            // Authenticated users...
-
-          }
-
-          ok(element);
-
-        });
-      }
+      "_controller": demo.messagesPage
     }
   };
 
   return routes;
+};
+
+/**
+ * PAGES
+ */
+
+demo.welcomePage = function() {
+  return new Promise(function(ok, err) {
+
+    // Grab the current user.
+    var account = dg.currentUser();
+
+    // Let's build a render element to render on the page...
+    var element = { };
+
+    switch (dg.config('theme').name) {
+
+      // "Out of the box" theme.
+      case 'ava':
+        if (account.isAnonymous()) {
+          element.intro = {
+            _theme: 'item_list',
+            _type: 'ol',
+            _items: [
+              dg.t('Acknowledge the demo looks boring'),
+              dg.t('Realize DrupalGap is a headless, empty canvas for app developers'),
+              dg.t('Get "out of the box" to explore the tool set and extensions')
+            ]
+          };
+        }
+        else {
+          var msg = dg.t('To support other CSS Frameworks (or no framework at all), the DrupalGap front end is very flexible.');
+          element.intro = { _markup: '<blockquote>' + msg + '</blockquote>' };
+        }
+
+        break;
+
+      // Core theme for Bootstrap.
+      case 'burrito':
+        var msg = dg.t('Instantly add a _bootstrap front end to your app with a _module extension for DrupalGap.', {
+          _bootstrap: dg.l(dg.t('Bootstrap'), 'http://getbootstrap.com/'),
+          _module: dg.l(dg.t('module'), 'http://drupalgap.org/project/bootstrap')
+        });
+        element.intro = { _markup: '<blockquote>' + msg + '</blockquote>' };
+        break;
+
+      // Core theme for Foundation.
+      case 'frank':
+        var msg = dg.t('Instantly add a _foundation front end to your app with a _module extension for DrupalGap.', {
+          _foundation: dg.l(dg.t('Foundation'), 'http://foundation.zurb.com/'),
+          _module: dg.l(dg.t('module'), 'http://drupalgap.org/project/foundation')
+        });
+        element.intro = { _markup: '<blockquote>' + msg + '</blockquote>' };
+        break;
+
+    }
+
+    // List out some of DrupalGap's best features and how to get started.
+    if (dg.config('theme').name != 'ava' || account.isAuthenticated()) {
+
+      element.features = {
+        _markup:
+
+        '<h2>' + dg.t('Tools and Features') + '</h2>' +
+        '<blockquote>' + dg.t('By utilizing familiar coding syntax and concepts from Drupal 8 such as...') + '</blockquote>' +
+        dg.theme('item_list', {
+          _items: [
+            dg.t('Entities / Fields'),
+            dg.t('Modules'),
+            dg.t('Hooks'),
+            dg.t('Themes'),
+            dg.t('Regions'),
+            dg.t('Blocks'),
+            dg.t('Templates'),
+            dg.t('Routes / Custom Pages'),
+            dg.t('Forms API'),
+            dg.t('User Roles / Permissions')
+          ]
+        }) +
+        '<blockquote>' + dg.t('with built in pages, widgets and forms to handle...') + '</blockquote>' +
+        dg.theme('item_list', {
+          _items: [
+            dg.t('User Authentication'),
+            dg.t('Adding / Editing Entities'),
+            dg.t('Displaying Entities / Fields'),
+            dg.t('Rendering Views Result Data')
+          ]
+        }) +
+        '<blockquote>' + dg.t('the DrupalGap tool set is dedicated to Drupal 8 application development.') + '</blockquote>' +
+
+        '<h2>' + dg.t('Continuing the Demo') + '</h2>' +
+        '<blockquote>' + dg.t('Be sure to say hello on the _map and browse the _messages list.', {
+          _map: dg.l(dg.t('map'), 'map'),
+          _messages: dg.l(dg.t('messages'), 'messages')
+        }) + '</blockquote>' +
+
+        '<h2>' + dg.t('Getting Started') + '</h2>' +
+        '<blockquote>' + dg.t('Try the _helloWorld World for DrupalGap.', {
+          _helloWorld: dg.l(dg.t('Hello World'), 'http://docs.drupalgap.org/8/Hello_World')
+        }) + '</blockquote>'
+      };
+
+    }
+
+    ok(element);
+
+  });
+};
+
+demo.mapPage = function() {
+  return new Promise(function(ok, err) {
+
+    // Grab the current user.
+    var account = dg.currentUser();
+
+    // Let's build a render element to render on the page...
+    var element = {};
+
+    // Google map.
+    element.map = demo.getMapRenderElement();
+    element.map._postRender.push(function() {
+
+      // Add clickable markers on the map for recent messages sent, then set the center of the map.
+      jDrupal.viewsLoad('articles').then(function(view) {
+        var results = view.getResults();
+        var markers = [];
+        for (var i = 0; i < results.length; i ++) {
+          var node = new jDrupal.Node(results[i]);
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(
+                node.get('field_latitude', 0).value,
+                node.get('field_longitude', 0).value
+            ),
+            map: demo.map,
+            icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+          });
+          markers.push(marker);
+          var content = dg.l(node.getTitle(), 'node/' + node.id());
+          var infowindow = new google.maps.InfoWindow();
+          google.maps.event.addListener(marker, 'click', (function(marker, content, infowindow) {
+            return function() {
+              infowindow.setContent(content);
+              infowindow.open(demo.map, marker);
+            };
+          })(marker, content, infowindow));
+        }
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0; i < markers.length; i++) { bounds.extend(markers[i].getPosition()); }
+        demo.map.fitBounds(bounds);
+      });
+
+    });
+
+    // Add a placeholder for showing messages.
+    element['message'] = {
+      _markup: '<div id="demo-message"></div>',
+      _weight: 2
+    };
+
+    // Anonymous users...
+    if (!account.isAuthenticated()) {
+
+      // Direct them to the login form so they can use the map.
+      element['message']._postRender = [function() {
+        var nid = window.localStorage.getItem('demo_message_sent');
+        demo.setMessage({
+          _theme: 'message',
+          _message: dg.t('Login to add a greeting to the map.')
+        });
+      }];
+      ok(element);
+
+    }
+
+    else {
+
+      // Authenticated users...
+
+      // If they've already sent a message, let them know and don't show the form.
+      if (window.localStorage.getItem('demo_message_sent')) {
+        element['message']._postRender = [function() {
+          var nid = window.localStorage.getItem('demo_message_sent');
+          demo.setMessage({
+            _theme: 'message',
+            _type: 'warning',
+            _message: dg.t('You already sent a message!') + ' (' + dg.l(dg.t('view message'), 'node/' + nid) + ')'
+          });
+        }];
+        ok(element);
+        return;
+      }
+
+      // Show a message informing the user how to use the map and form.
+      element['message']._postRender = [function() {
+        demo.setMessage({
+          _theme: 'message',
+          _type: 'info',
+          _message:
+          dg.t('Click on the map and enter your message below...') + ' ' +
+          dg.l(dg.t('or use your current location'), null, {
+            _attributes: {
+              href: '',
+              onclick: 'demo.getCurrentLocation(); return false;'
+            }
+          })
+        });
+      }];
+
+      // Load the form, add it to DrupalGap, then attach its html to our render element.
+      dg.addForm('DemoSayHelloForm', dg.applyToConstructor(DemoSayHelloForm)).getForm().then(function(formHTML) {
+        element['say_hello_form'] = {
+          _markup: formHTML,
+          _weight: 2
+        };
+        ok(element);
+      });
+
+    }
+
+  });
+};
+
+demo.messagesPage = function() {
+  return new Promise(function(ok, err) {
+
+    // Display a View listing of recent greetings.
+    var element = {};
+    element['article_list'] = dg.recentGreetings();
+    ok(element);
+
+  });
 };
 
 /**
@@ -328,6 +309,11 @@ var DemoSwitchForm = function() {
           onchange: "demo.switchFramework(this)"
         }
       };
+
+      // Add a css class when out of the box.
+      if (demo.outOfTheBox() && dg.currentUser().isAnonymous()) {
+        form.css_frameworks._attributes.class = ['out-of-the-box'];
+      }
 
       // Send the form back to be rendered.
       ok(form);
@@ -457,12 +443,24 @@ function demo_block_view_alter(element, block) {
 
   switch (block.get('id')) {
 
-    // Add a link to the main menu.
+
     case 'main_menu':
-      element.menu._items.push(
-          dg.l(dg.t('Map'), 'map'),
-          dg.l(dg.t('Messages'), 'messages')
-      );
+
+        // Clear the main main for anonymous users, out of the box.
+        if (dg.config('theme').name == 'ava' && dg.currentUser().isAnonymous()) {
+          delete element.menu;
+          return;
+        }
+
+        // If it's bootstrap, we can drop the "Home" link (we actually clear it here, so careful).
+        if (dg.config('theme').name == 'burrito') { element.menu._items = []; }
+
+        // Add links to the main menu.
+        element.menu._items.push(
+            dg.l(dg.t('Map'), 'map'),
+            dg.l(dg.t('Messages'), 'messages')
+        );
+
       break;
 
   }
@@ -739,3 +737,9 @@ demo.getCurrentLocation = function() {
 
   );
 };
+
+/**
+ * Returns true if the app is in the out of the box mode, aka running the "ava" theme, false otherwise.
+ * @returns {boolean}
+ */
+demo.outOfTheBox = function() { return dg.config('theme').name == 'ava' };
